9357d61de9c59f19bc08f6d7e2c8a87e7b9575e2
Sophie Janson <sophie@sophiejanson.com>
2018-06-08 21:43:33 +0200

    added addItem method

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,9 +1,21 @@
 class ShoppingCart {
   constructor() {
+    this._items = []
   }
+
   getItems() {
-    return []
+    return this._items
   }
+
+  addItem(itemName, quantity, price) {
+    this._items.push({
+      name: itemName,
+      quantity: quantity,
+      pricePerUnit: price
+    })
+  }
+
+
 }
 
 module.exports.ShoppingCart = ShoppingCart
