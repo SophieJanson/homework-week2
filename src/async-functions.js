5ac3bfffaa3b4af2a4b4fbe96b6bbdf56e0814b8
Sophie Janson <sophie@sophiejanson.com>
2018-06-10 09:53:33 +0200

    Passed first two async-functions tests

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,7 +1,25 @@
-function giveItBackLater() {}
-function addSomePromises() {}
-function promiseToGiveItBackLater() {}
+function giveItBackLater(value, callback) {
+  setTimeout(callback.bind(null, value), 5000)
+}
 
+giveItBackLater("pink", function(val) {
+  console.log(val, "elephant");
+})
+
+function addSomePromises() {}
+
+function promiseToGiveItBackLater(value) {
+  return new Promise((resolve, reject) => {
+    if(!value) {
+      return reject("Value not found")
+    }
+    return resolve(value)
+  })
+}
+
+promiseToGiveItBackLater("elephant")
+  .then(val => console.log(val + " is pink."))
+  .catch(err => console.log(err))
 
 module.exports = {
   giveItBackLater,
